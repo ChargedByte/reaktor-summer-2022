9a71e1f2ba38a3b18902c2205150f36974b3e2a4
Peetu Saarinen <root@chargedbyte.dev>
2022-01-31 18:01:52 +0200

    Add footer content

--- client/components/AppFooter.vue
+++ client/components/AppFooter.vue
@@ -1,5 +1,26 @@
 <template>
-  <v-footer app></v-footer>
+  <v-footer app>
+    <span
+      >Icons
+      <a href="https://icons8.com/icon/16463/rock" target="_blank">Rock</a>,
+      <a href="https://icons8.com/icon/13580/paper" target="_blank">Paper</a>,
+      <a href="https://icons8.com/icon/85716/cutting" target="_blank"
+        >Cutting</a
+      >
+      by <a href="https://icons8.com" target="_blank">Icons8</a></span
+    >
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/ChargedByte/reaktor-summer-2022"
+      icon
+      small
+      target="_blank"
+    >
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
+  </v-footer>
 </template>
 
 <script>
