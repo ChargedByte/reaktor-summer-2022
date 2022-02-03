571ade982b7c0527d8a30900bae9ba5300a6d3bf
Peetu Saarinen <root@chargedbyte.dev>
2022-02-03 07:42:54 +0200

    Add StatusPages to server

--- server/src/main/kotlin/dev/chargedbyte/reaktor_summer_2022/plugins/Routing.kt
+++ server/src/main/kotlin/dev/chargedbyte/reaktor_summer_2022/plugins/Routing.kt
@@ -1,8 +1,11 @@
 package dev.chargedbyte.reaktor_summer_2022.plugins
 
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.locations.*
 
 fun Application.configureRouting() {
     install(Locations)
+
+    install(StatusPages)
 }
